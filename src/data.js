df6d028514db951c04d7a7c6c6eee55fa37b66c4
massmr <marcellin.massimo@gmail.com>
2024-01-27 08:05:56 +0100

    data add

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const people = [{
+  name: 'John Doe',
+  age: 54,
+  hairColor: 'brown',
+  hobbies: ['swimming', 'bicycle', 'video games']
+}, {
+  name: 'Martin Matin',
+  age: 32,
+  hairColor: 'black',
+  hobbies: ['golf', 'mathematics']
+}, {
+  name: 'Jane Garcia',
+  age: 24,
+  hairColor: 'blonde',
+  hobbies: ['biology', 'medecine', 'gymnastics']
+}];
+
+export const products = [{
+  name: 'Flat-screen TV',
+  price: '$300',
+  description: 'Huge LCD screen, a great deal',
+  rating: 4.5,
+}, {
+  name: 'Basketball',
+  price: '$25',
+  description: 'Just like the pos use',
+  rating: 3.8,
+}, {
+  name: 'Running shoes',
+  price: '$120',
+  description: 'State-of-the-art technology for optimum running',
+  rating: 4.2,
+}];
